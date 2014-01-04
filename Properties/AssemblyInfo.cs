8f677a05b1d403cff1fd98e0f8be4d534bc813f3
fotisgpap@grapto.com <fotisgpap@grapto.com>
2014-01-04 16:18:49 +0200

    1.5.2.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Security;
 
 [assembly: Guid("EAB6449E-AE23-48EE-837D-5FF43E628C68")]
 
-[assembly: AssemblyVersion("1.5.2.2")]
-[assembly: AssemblyFileVersion("1.5.2.2")]
+[assembly: AssemblyVersion("1.5.2.3")]
+[assembly: AssemblyFileVersion("1.5.2.3")]
 
 [assembly: AssemblyCompanyAttribute("Team Grapto (tweet: @teamgrapto)")]
